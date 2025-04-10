8d0ff75bc2329afd294a3848bae4c5229a88a92d
Dev Selimcan <selimcangursu@asemsoft.com>
2025-04-10 15:04:27 +0300

    footer alanı içerikleri düzenlendi.

--- admin/resources/views/partials/footer.blade.php
+++ admin/resources/views/partials/footer.blade.php
@@ -1,26 +1,18 @@
 		<footer class="footer">
-				<div class="container-fluid">
-					<nav class="pull-left">
-						<ul class="nav">
-							<li class="nav-item">
-								<a class="nav-link" href="http://www.themekita.com">
-									ThemeKita
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">
-									Help
-								</a>
-							</li>
-							<li class="nav-item">
-								<a class="nav-link" href="#">
-									Licenses
-								</a>
-							</li>
-						</ul>
-					</nav>
-					<div class="copyright ms-auto">
-						2024, made with <i class="fa fa-heart heart text-danger"></i> by <a href="http://www.themekita.com">ThemeKita</a>
-					</div>				
-				</div>
-			</footer>
+		  <div class="container-fluid">
+		    <nav class="pull-left">
+		      <ul class="nav">
+		        <li class="nav-item">
+		          <a class="nav-link" href="http://www.themekita.com"> Destek </a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="#"> Sıkça Sorulan Sorular </a>
+		        </li>
+		        <li class="nav-item">
+		          <a class="nav-link" href="#"> Dokumantasyon </a>
+		        </li>
+		      </ul>
+		    </nav>
+		    <div class="copyright ms-auto text-muted"> © 2025 | Tüm hakları saklıdır. Bu admin paneli <a href="#" target="_blank" class="text-decoration-none">Selimcan Gürsu</a> tarafından tasarlanmıştır. </div>
+		  </div>
+		</footer>
