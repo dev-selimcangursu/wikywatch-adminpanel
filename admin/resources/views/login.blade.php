91aa94864e200be3e003253f5c65a0481c9ea363
Dev Selimcan <selimcangursu@asemsoft.com>
2025-04-10 14:24:03 +0300

    login.blade.php sayfası oluşturuldu.

---
+++ admin/resources/views/login.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+    <title>Giriş Yap | Wiky Watch Admin Panel</title>
+  </head>
+  <body>
+    <div class="container text-center">
+      <img class="mt-5" src="" alt="wiky-watch-logo">
+      <div class="card d-flex m-auto w-50 justify-content-center mt-3">
+        <div class="card-header text-center">
+          <h5 class="card-title"> Giriş Yap </h3>
+        </div>
+        <div class="card-body text-start">
+          <form>
+            <div class="mb-3">
+              <label for="email" class="form-label">E-Posta Adresi</label>
+              <input type="email" class="form-control" id="email">
+            </div>
+            <div class="mb-3">
+              <label for="password" class="form-label">Parola</label>
+              <input type="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary w-100">Giriş Yap</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
--- admin/routes/web.php
+++ admin/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('dashboard');
-});
+
+Route::get('/dashboard',[DashboardController::class,'index']);
+Route::get('/',[LoginController::class,'index']);
