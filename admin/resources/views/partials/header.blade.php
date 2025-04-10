5aa6c036bbe5f4c448b2fc24ffad5d04457f5e18
Dev Selimcan <selimcangursu@asemsoft.com>
2025-04-10 15:01:31 +0300

    header.blade.php sayfası içerikleri düzenlendi.

--- admin/resources/views/partials/header.blade.php
+++ admin/resources/views/partials/header.blade.php
@@ -23,19 +23,7 @@
 				</div>
 				<!-- Navbar Header -->
 				<nav class="navbar navbar-header navbar-header-transparent navbar-expand-lg border-bottom">
-
 					<div class="container-fluid">
-						<nav class="navbar navbar-header-left navbar-expand-lg navbar-form nav-search p-0 d-none d-lg-flex">
-							<div class="input-group">
-								<div class="input-group-prepend">
-									<button type="submit" class="btn btn-search pe-1">
-										<i class="fa fa-search search-icon"></i>
-									</button>
-								</div>
-								<input type="text" placeholder="Search ..." class="form-control">
-							</div>
-						</nav>
-
 						<ul class="navbar-nav topbar-nav ms-md-auto align-items-center">
 							<li class="nav-item topbar-icon dropdown hidden-caret d-flex d-lg-none">
 								<a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false" aria-haspopup="true">
@@ -177,72 +165,7 @@
 									</li>
 								</ul>
 							</li>
-							<li class="nav-item topbar-icon dropdown hidden-caret">
-								<a class="nav-link" data-bs-toggle="dropdown" href="#" aria-expanded="false">
-									<i class="fas fa-layer-group"></i>
-								</a>
-								<div class="dropdown-menu quick-actions animated fadeIn">
-									<div class="quick-actions-header">
-										<span class="title mb-1">Quick Actions</span>
-										<span class="subtitle op-7">Shortcuts</span>
-									</div>
-									<div class="quick-actions-scroll scrollbar-outer">
-										<div class="quick-actions-items">
-											<div class="row m-0">
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-danger rounded-circle">
-															<i class="far fa-calendar-alt"></i>
-														</div>
-														<span class="text">Calendar</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-warning rounded-circle">
-															<i class="fas fa-map"></i>
-														</div>
-														<span class="text">Maps</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-info rounded-circle">
-															<i class="fas fa-file-excel"></i>
-														</div>
-														<span class="text">Reports</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-success rounded-circle">
-															<i class="fas fa-envelope"></i>
-														</div>
-														<span class="text">Emails</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-primary rounded-circle">
-															<i class="fas fa-file-invoice-dollar"></i>
-														</div>
-														<span class="text">Invoice</span>
-													</div>
-												</a>
-												<a class="col-6 col-md-4 p-0" href="#">
-													<div class="quick-actions-item">
-														<div class="avatar-item bg-secondary rounded-circle">
-															<i class="fas fa-credit-card"></i>
-														</div>
-														<span class="text">Payments</span>
-													</div>
-												</a>
-											</div>
-										</div>
-									</div>
-								</div>
-							</li>
-							
+						
 							<li class="nav-item topbar-user dropdown hidden-caret">
 								<a class="dropdown-toggle profile-pic" data-bs-toggle="dropdown" href="#" aria-expanded="false">
 									<div class="avatar-sm">
@@ -259,19 +182,16 @@
 												<div class="avatar-lg"><img src="assets/img/profile.jpg" alt="image profile" class="avatar-img rounded"></div>
 												<div class="u-text">
 													<h4>Hizrian</h4>
-													<p class="text-muted">hello@example.com</p><a href="profile.html" class="btn btn-xs btn-secondary btn-sm">View Profile</a>
+													<p class="text-muted">hello@example.com</p><a href="profile.html" class="btn btn-xs btn-secondary btn-sm">Profilimi Görüntüle</a>
 												</div>
 											</div>
 										</li>
 										<li>
 											<div class="dropdown-divider"></div>
-											<a class="dropdown-item" href="#">My Profile</a>
-											<a class="dropdown-item" href="#">My Balance</a>
-											<a class="dropdown-item" href="#">Inbox</a>
+											<a class="dropdown-item" href="#">Gelen Kutusu</a>
+											<a class="dropdown-item" href="#">Bildirimler</a>
 											<div class="dropdown-divider"></div>
-											<a class="dropdown-item" href="#">Account Setting</a>
-											<div class="dropdown-divider"></div>
-											<a class="dropdown-item" href="#">Logout</a>
+											<a class="dropdown-item" href="#">Çıkış Yap</a>
 										</li>
 									</div>
 								</ul>
